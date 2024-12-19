4e9709a47f7f21a83be8bc30ae28e71eca3acb4b
EstebanGZ-maker <estebangz070@gmail.com>
2024-12-19 11:55:56 -0500

    PRT-07-1

--- front-end/src/api/auth.js
+++ front-end/src/api/auth.js
@@ -2,6 +2,6 @@ import axios  from "./axios.js";
 
 export const registerRequest = (user) => axios.post( `/register`, user); 
 
-export const loginRequest = (user)=> axios.post( `/login`, user); 
+export const loginRequest = (user)=> axios.post( `/auth/login`, user); 
 
 export const verifyTokenRequet = () => axios.get(`/verify`)
