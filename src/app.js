8910d15bc3646ef8b71d705b7524c2c3b2a05ad8
EstebanGZ-maker <estebangz070@gmail.com>
2025-01-02 10:09:24 -0500

    PRT-09

--- src/app.js
+++ src/app.js
@@ -15,6 +15,13 @@ const app = express();
 
 //Middlewares
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://consultascadi.up.railway.app'); // Allow requests from your frontend
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow specific headers
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+    next();
+});
+
 app.use(cors({
     origin: [process.env.CLIENT_URL, 'http://localhost:5173'], // URLs permitidas
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
